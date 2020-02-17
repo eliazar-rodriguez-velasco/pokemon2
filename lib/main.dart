5521c8000605c1ee91ed3c063fc86619397b4d07
Emma Romero <emmayrs17@gmail.com>
2020-02-16 21:05:00 -0600

    imagen y nombre listo

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,11 @@ import 'package:pokemon2/pokemon.dart';
 import 'dart:convert';
 
 void main() => runApp(MaterialApp(
-  title:"Pokemon App",
-  home: HomePage(),
-  theme: ThemeData(primarySwatch: Colors.teal),
-  debugShowCheckedModeBanner: false,
-));
+      title: "Pokemon App",
+      home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.teal),
+      debugShowCheckedModeBanner: false,
+    ));
 
 class HomePage extends StatefulWidget {
   @override
@@ -16,15 +16,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var url="https://raw.githubusercontent.com/emmaRomero19/flutter/master/pokemon/pokemon.json";
+  var url =
+      "https://raw.githubusercontent.com/emmaRomero19/flutter/master/pokemon/pokemon.json";
   PokeHub pokeHub;
+
   @override
-  void initState(){
+  void initState() {
     super.initState();
     fetchData();
     print("2nd work");
   }
-  fetchData()async{
+
+  fetchData() async {
     var res = await http.get(url);
     var decodedValue = jsonDecode(res.body);
     pokeHub = PokeHub.fromJson(decodedValue);
@@ -33,34 +36,44 @@ class _HomePageState extends State<HomePage> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
+      appBar: AppBar(
         title: Text("Pokemon App"),
         backgroundColor: Colors.cyan,
       ),
-
       body: GridView.count(
         crossAxisCount: 2,
-        children: pokeHub.pokemon.map((Pokemon poke)=>Padding(
-          padding: const EdgeInsets.all(2.0),
-          child: Card(
-            elevation:3.0,
-            child:Column(
-              children: <Widget>[
-                Container(
-
-                ),
-              ],
-            ),
-          ),
-        )).toList(),
+        children: pokeHub.pokemon
+            .map((Pokemon poke) => Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Card(
+                    elevation: 3.0,
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          height: 100.0,
+                          width: 100.0,
+                          decoration: BoxDecoration(
+                            image:
+                                DecorationImage(image: NetworkImage(poke.img)),
+                          ),
+                        ),
+                        Text(
+                          poke.name,
+                          style: TextStyle(
+                              fontSize: 20.0, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ),
+                ))
+            .toList(),
       ),
       drawer: Drawer(),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: () {},
         backgroundColor: Colors.cyan,
-        child:Icon(Icons.refresh),
+        child: Icon(Icons.refresh),
       ),
     );
   }
 }
-
