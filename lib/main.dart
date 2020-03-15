8b5ea2188eede6a5c0c7d2218c9a49ad0022ca62
Emma Romero <emmayrs17@gmail.com>
2020-03-15 11:58:10 -0600

    pokemon busqueda y rotacion

--- lib/main.dart
+++ lib/main.dart
@@ -6,135 +6,161 @@ import 'package:pokemon2/pokemon.dart';
 import 'dart:convert';
 
 void main() => runApp(MaterialApp(
-      title: "Pokemon App",
-      home: HomePage(),
-      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Carter'),
-      debugShowCheckedModeBanner: false,
-    ));
+    title: "Pokemon App",
+    home: HomePage(),
+    theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Carter'),
+    debugShowCheckedModeBanner: false,
+));
 
 class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
+    @override
+    _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  var url =
-      "https://raw.githubusercontent.com/Adrian-Cruz-SanJuan/pokedex_yuko/master/pokemon.json";
-  PokeHub pokeHub;
+    var url =
+        "https://raw.githubusercontent.com/Adrian-Cruz-SanJuan/pokedex_yuko/master/pokemon.json";
+    PokeHub pokeHub;
 
-  @override
-  void initState() {
-    super.initState();
-    fetchData();
-  }
+    @override
+    void initState() {
+        super.initState();
+        fetchData();
+    }
 
-  void fetchData() async {
-    var res = await http.get(url);
-    var decodedValue = jsonDecode(res.body);
-    pokeHub = PokeHub.fromJson(decodedValue);
-    setState(() {});
-  }
+    void fetchData() async {
+        var res = await http.get(url);
+        var decodedValue = jsonDecode(res.body);
+        pokeHub = PokeHub.fromJson(decodedValue);
+        setState(() {});
+    }
 
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Pokemon App"),
-        backgroundColor: Colors.greenAccent,
-      ),
-      body: pokeHub == null
-          ? Center(
-              child: CircularProgressIndicator(),
+    /*Control de Busqueda*/
+    String _searchText = "";
+    final TextEditingController _search = new TextEditingController();
+    Widget _appBarTitle = new Text("Pokemon");
+    bool _typing = false;
+
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: _typing ? TextField(
+                    autofocus: true,
+                    controller: _search,
+                    onChanged: (text){
+                        setState(() {});
+                    },
+                ):Text("Pokemon"),
+                leading: IconButton(
+                    icon: Icon(_typing ? Icons.done: Icons.search),
+                    onPressed: () {
+                        print("Is typing: "+ _typing.toString());
+                        setState(() {
+                            _typing = !_typing;
+                            _search.text = "";
+                        });
+                    },
+                ),
+                centerTitle: true,
+                backgroundColor: Colors.greenAccent,
+            ),
+            body: pokeHub == null
+                ? Center(
+                child: CircularProgressIndicator(),
             )
-          : GridView.count(
-              crossAxisCount: 2,
-              children: pokeHub.pokemon
-                  .map((Pokemon poke) => Padding(
+                : OrientationBuilder(
+            builder: (context, orientation) {
+            return GridView.count(
+                crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
+                children: pokeHub.pokemon
+                    .where((poke) =>
+                ((poke.name
+                    .toLowerCase()
+                    .contains(_search.text.toLowerCase())) ||
+                    poke.type.toList().toList().toString()
+                        .toLowerCase()
+                        .contains(_search.text.toLowerCase()) ||
+                    poke.num.toString().contains(_search.text)
+                ))
+                    .map((Pokemon poke) =>
+                    Padding(
                         padding: const EdgeInsets.all(2.0),
                         child: InkWell(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => PokeDetail(
-                                          pokemon: poke,
+                            onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => PokeDetail(
+                                            pokemon: poke,
                                         )));
-                          },
-                          child: Card(
-                            elevation: 3.0,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: <Widget>[
-                                Hero(
-                                  tag: poke.img,
-                                  child: Container(
-                                    height: 100.0,
-                                    width: 100.0,
-                                    decoration: BoxDecoration(
-                                      image: DecorationImage(
-                                          image: NetworkImage(poke.img)),
-                                    ),
-                                  ),
+                            },
+                            child: Card(
+                                elevation: 3.0,
+                                child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                    children: <Widget>[
+                                        Hero(
+                                            tag: poke.img,
+                                            child: Container(
+                                                height: 100.0,
+                                                width: 100.0,
+                                                decoration: BoxDecoration(
+                                                    image: DecorationImage(
+                                                        image: NetworkImage(poke.img)),
+                                                ),
+                                            ),
+                                        ),
+                                        Text(
+                                            poke.name,
+                                            style: TextStyle(
+                                                fontSize: 20.0,
+                                                fontWeight: FontWeight.bold),
+                                        )
+                                    ],
                                 ),
-                                Text(
-                                  poke.name,
-                                  style: TextStyle(
-                                      fontSize: 20.0,
-                                      fontWeight: FontWeight.bold),
-                                )
-                              ],
                             ),
-                          ),
                         ),
-                      ))
-                  .toList(),
+                    ))
+                    .toList(),
+            );},),
+            endDrawer: Drawer(
+                elevation: 16.0,
+                child: ListView(
+                    children: const <Widget>[
+                        DrawerHeader(
+                            child: Text("Pokemon App",
+                                style: TextStyle(fontFamily: 'Carter', fontSize: 30)),
+                            decoration: BoxDecoration(color: Colors.greenAccent),
+                        ),
+                        ListTile(
+                            title: Text(
+                                "Romero Sosa Emma Yuridia",
+                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                            ),
+                        ),
+                        ListTile(
+                            title: Text(
+                                "Rodriguez Velasco Eliazar",
+                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                            )),
+                        ListTile(
+                            title: Text(
+                                "Leon Barron Adolfo",
+                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                            )),
+                        ListTile(
+                            title: Text(
+                                "Cruz San Juan Adrian",
+                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                            )),
+                        ListTile(
+                            title: Text(
+                                "Muñoz Ruíz Nazareth",
+                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                            ))
+                    ],
+                ),
             ),
-      drawer: Drawer(
-        child: ListView(
-          children: const <Widget>[
-            DrawerHeader(
-              child: Text("Pokemon App",
-                  style: TextStyle(fontFamily: 'Carter', fontSize: 30)),
-              decoration: BoxDecoration(color: Colors.greenAccent),
-            ),
-            ListTile(
-                title: Text(
-              "Romero Sosa Emma Yuridia",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-            )),
-            ListTile(
-                title: Text(
-              "Rodriguez Velasco Eliazar",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-            )),
-            ListTile(
-                title: Text(
-              "Leon Barron Adolfo",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-            )),
-            ListTile(
-                title: Text(
-              "Cruz San Juan Adrian",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-            )),
-            ListTile(
-                title: Text(
-              "Muñoz Ruíz Nazareth",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-            )),
-            ListTile(
-                title: Text(
-                  "Meneses Alegria Diana Marlen",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-                )),
-            ListTile(
-                title: Text(
-                  "Luis Hernandez Oliveira",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-                ))
-
-          ],
-        ),
-      ),
-    );
-  }
+        );
+    }
 }
--- lib/pokedetail.dart
+++ lib/pokedetail.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:pokemon2/main.dart';
 import 'package:pokemon2/pokemon.dart';
+import 'pokemon.dart';
+import 'package:pokemon2/main.dart';
 
 class PokeDetail extends StatelessWidget {
   final Pokemon pokemon;
@@ -8,149 +11,162 @@ class PokeDetail extends StatelessWidget {
 
   bodyWidget(BuildContext context) => Stack(
         children: <Widget>[
-          Positioned(
-            height: MediaQuery.of(context).size.height * 2,
-            width: MediaQuery.of(context).size.width-20,
-            left: 10.0,
-            top: MediaQuery.of(context).size.height * 0.05,
+          Container(
+            height: MediaQuery.of(context).size.height,
             child: SingleChildScrollView(
-                child: Card(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25.0)),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        SizedBox(
-                          height: 150.0,
-                        ),
-                        Text(pokemon.name,
-                            style: TextStyle(
-                                fontSize: 30.0, fontWeight: FontWeight.bold)),
-                        Text("Height: ${pokemon.height}",
-                            style: TextStyle(
-                                fontFamily: 'PTS',
-                                fontSize: 16.0,
-                                fontStyle: FontStyle.italic)),
-                        Text("Weight: ${pokemon.weight}",
-                            style: TextStyle(
-                                fontFamily: 'PTS',
-                                fontSize: 16.0,
-                                fontStyle: FontStyle.italic)),
-                        Text("Types",
-                            style: TextStyle(
-                                fontSize: 25.0, fontWeight: FontWeight.bold)),
-                        new Wrap(
-                          spacing: 8.0,
-                          runSpacing: 4.0,
-                          direction: Axis.horizontal,
-                          children: pokemon.type
-                              .map((t) => FilterChip(
-                                  label: Text(t,
-                                      style: TextStyle(
-                                          fontFamily: 'PTS',
-                                          fontSize: 14.0,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white)),
-                                  backgroundColor: Colors.green[600],
-                                  onSelected: (b) {}))
-                              .toList(),
-                        ),
-                        Text("Weakness",
-                            style: TextStyle(
-                                fontSize: 22.0, fontWeight: FontWeight.bold)),
-                        new Wrap(
-                          spacing: 8.0,
-                          runSpacing: 4.0,
-                          direction: Axis.horizontal,
-                          children: pokemon.weaknesses
-                              .map((t) => FilterChip(
-                                  label: Text(
-                                    t,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(60.0)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[                    Align(
+                    alignment: Alignment.topCenter,
+                    child: Hero(
+                        tag: pokemon.img,
+                        child: Container(
+                          height: 200.0,
+                          width: 200.0,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: NetworkImage(pokemon.img),
+                                fit: BoxFit.cover),
+                          ),
+                        )),
+                  ),
+
+                    Text(pokemon.name,
+                        style: TextStyle(
+                            fontSize: 30.0, fontWeight: FontWeight.bold)),
+                    Text("Height: ${pokemon.height}",
+                        style: TextStyle(
+                            fontFamily: 'PTS',
+                            fontSize: 16.0,
+                            fontStyle: FontStyle.italic)),
+                    Text("Weight: ${pokemon.weight}",
+                        style: TextStyle(
+                            fontFamily: 'PTS',
+                            fontSize: 16.0,
+                            fontStyle: FontStyle.italic)),
+                    Text("Types",
+                        style: TextStyle(
+                            fontSize: 25.0, fontWeight: FontWeight.bold)),
+                    new Container(
+                      width: MediaQuery.of(context).size.width - 45,
+                      padding: EdgeInsets.only(left: 40.0, right: 40.0),
+                      child: new Wrap(
+                        spacing: 10.0,
+                        direction: Axis.horizontal,
+                        children: pokemon.type
+                            .map((t) => FilterChip(
+                                label: Text(t,
                                     style: TextStyle(
                                         fontFamily: 'PTS',
                                         fontSize: 14.0,
                                         fontWeight: FontWeight.bold,
-                                        color: Colors.white),
-                                  ),
-                                  backgroundColor: Colors.red,
-                                  onSelected: (b) {}))
-                              .toList(),
-                        ),
-                        Text("Prev-Evolution",
-                            style: TextStyle(
-                                fontSize: 22.0, fontWeight: FontWeight.bold)),
-                        new Wrap(
-                          spacing: 8.0,
-                          runSpacing: 4.0,
-                          direction: Axis.horizontal,
-                          children: pokemon.prevEvolution == null
-                              ? <Widget>[
-                                  Text("This is the first form",
-                                      style: TextStyle(
-                                          fontFamily: 'PTS',
-                                          fontSize: 14.0,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white))
-                                ]
-                              : pokemon.prevEvolution
-                                  .map((n) => FilterChip(
-                                        backgroundColor: Colors.orangeAccent,
-                                        label: Text(
-                                          n.name,
-                                          style: TextStyle(
-                                              fontFamily: 'PTS',
-                                              fontSize: 14.0,
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.black),
-                                        ),
-                                        onSelected: (b) {},
-                                      ))
-                                  .toList(),
-                        ),
-                        Text("Next-Evolution",
-                            style: TextStyle(
-                                fontSize: 22.0, fontWeight: FontWeight.bold)),
-                        new Wrap(
-                          spacing: 8.0,
-                          runSpacing: 4.0,
-                          direction: Axis.horizontal,
-                          children: pokemon.nextEvolution == null
-                              ? <Widget>[
-                                  Text("This is the final form",
-                                      style: TextStyle(
-                                          fontFamily: 'PTS',
-                                          fontSize: 14.0,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white))
-                                ]
-                              : pokemon.nextEvolution
-                                  .map((n) => FilterChip(
-                                        backgroundColor: Colors.lightGreen,
-                                        label: Text(
-                                          n.name,
-                                          style: TextStyle(color: Colors.white),
-                                        ),
-                                        onSelected: (b) {},
-                                      ))
-                                  .toList(),
-                        ),
-                      ],
-                    )),
-                scrollDirection: Axis.vertical,
-                reverse: false,),
-          ),
-          Align(
-            alignment: Alignment.topCenter,
-            child: Hero(
-                tag: pokemon.img,
-                child: Container(
-                  height: 200.0,
-                  width: 200.0,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: NetworkImage(pokemon.img), fit: BoxFit.cover),
-                  ),
-                )),
+                                        color: Colors.white)),
+                                backgroundColor: Colors.green[600],
+                                onSelected: (b) {}))
+                            .toList(),
+                      ),
+                    ),
+                    Text("Weakness",
+                        style: TextStyle(
+                            fontSize: 22.0, fontWeight: FontWeight.bold)),
+                    new Container(
+                      width: MediaQuery.of(context).size.width - 45,
+                      padding: EdgeInsets.only(left: 40.0, right: 40.0),
+                      child: new Wrap(
+                        spacing: 10.0,
+                        runSpacing: 4.0,
+                        direction: Axis.horizontal,
+                        children: pokemon.weaknesses
+                            .map((t) => FilterChip(
+                                label: Text(
+                                  t,
+                                  style: TextStyle(
+                                      fontFamily: 'PTS',
+                                      fontSize: 14.0,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white),
+                                ),
+                                backgroundColor: Colors.red,
+                                onSelected: (b) {}))
+                            .toList(),
+                      ),
+                    ),
+                    Text("Prev-Evolution",
+                        style: TextStyle(
+                            fontSize: 22.0, fontWeight: FontWeight.bold)),
+                    new Container(
+                      width: MediaQuery.of(context).size.width - 45,
+                      padding: EdgeInsets.only(left: 40.0, right: 40.0),
+                      child: new Wrap(
+                        spacing: 8.0,
+                        runSpacing: 4.0,
+                        direction: Axis.horizontal,
+                        children: pokemon.prevEvolution == null
+                            ? <Widget>[
+                                Text("This is the first form",
+                                    style: TextStyle(
+                                        fontFamily: 'PTS',
+                                        fontSize: 14.0,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white))
+                              ]
+                            : pokemon.prevEvolution
+                                .map((n) => FilterChip(
+                                      backgroundColor: Colors.orangeAccent,
+                                      label: Text(
+                                        n.name,
+                                        style: TextStyle(
+                                            fontFamily: 'PTS',
+                                            fontSize: 14.0,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.black),
+                                      ),
+                                      onSelected: (b) {},
+                                    ))
+                                .toList(),
+                      ),
+                    ),
+                    Text("Next-Evolution",
+                        style: TextStyle(
+                            fontSize: 22.0, fontWeight: FontWeight.bold)),
+                    new Container(
+                      width: MediaQuery.of(context).size.width - 45,
+                      padding: EdgeInsets.only(left: 50.0, right: 40.0),
+                      child: new Wrap(
+                        spacing: 8.0,
+                        runSpacing: 4.0,
+                        direction: Axis.horizontal,
+                        children: pokemon.nextEvolution == null
+                            ? <Widget>[
+                                Text("This is the final form",
+                                    style: TextStyle(
+                                        fontFamily: 'PTS',
+                                        fontSize: 14.0,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white))
+                              ]
+                            : pokemon.nextEvolution
+                                .map((n) => FilterChip(
+                                      backgroundColor: Colors.lightGreen,
+                                      label: Text(
+                                        n.name,
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                      onSelected: (b) {},
+                                    ))
+                                .toList(),
+                      ),
+                    ),
+
+                  ],
+                ),
+              ),
+              scrollDirection: Axis.vertical,
+              reverse: false,
+            ),
           ),
         ],
       );
@@ -166,6 +182,12 @@ class PokeDetail extends StatelessWidget {
         backgroundColor: Colors.greenAccent,
         title: Text(pokemon.name),
         elevation: 2.0,
+        leading: IconButton(
+          icon: Icon(Icons.home),
+          onPressed: () {
+            Navigator.pop(context, MaterialPageRoute(builder: (context) => HomePage()));
+          },
+        ),
       ),
       body: bodyWidget(context),
     );
